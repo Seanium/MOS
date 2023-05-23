9a78bab288ad22d9bbe8e3f0fe6b35a2ccdf73e5
管骁 <21373181@buaa.edu.cn>
2023-05-23 14:16:14 +0800

    finish tree

--- user/tree.c
+++ user/tree.c
@@ -1,46 +1,41 @@
 #include <lib.h>
 
 void print_tab(int num) {
-    for(int i = 0; i < num; i++)
-    {
+    for (int i = 0; i < num; i++) {
         fprintf(1, "    ");
     }
 }
 
 void get_tree(int depth, char *path) {
     int r;
-    if((r = open(path, O_RDONLY)) < 0) {
+    if ((r = open(path, O_RDONLY)) < 0) {
         user_panic("tree open dir failed");
     }
 
     struct Fd *fd = (struct Fd *) num2fd(r);
-    struct Filefd *fileFd;
-    fileFd = (struct FileFd *) fd;
+    struct Filefd *f = (struct Filefd *) fd;
 
-    fprintf(1, "\033[34m%s\033[0m\n", fileFd->f_file.f_name);
+    fprintf(1, "\033[34m%s\033[0m\n", f->f_file.f_name);
     
-    u_int num = ROUND(fileFd->f_file.f_size, sizeof(struct File)) / sizeof(struct File);
+    u_int num = ROUND(f->f_file.f_size, sizeof(struct File)) / sizeof(struct File);
     struct File *file = (struct File *) fd2data(fd);
 
-    for(int i = 0; i < num; i++)
-    {
-        if(file[i].f_name[0] == '\0') continue;
+    for (int i = 0; i < num; i++) {
+        if (file[i].f_name[0] == '\0') {
+	    continue;
+	}
         print_tab(depth);
         fprintf(1, "|---");
-        if (file[i].f_type == FTYPE_DIR)
-        {
+        if (file[i].f_type == FTYPE_DIR) {
             char newpath[MAXPATHLEN];
-
             strcpy(newpath, path);
             int len = strlen(path);
-            if(newpath[len - 1] != '/') {
-                *(newpath + len++) = '/';
+            if (newpath[len - 1] != '/') {
+                newpath[len++] = '/';
             }
             strcpy(newpath + len, file[i].f_name);
             get_tree(depth + 1, newpath);
-        }
-        else
-        {
+        } else {
             fprintf(1, "%s\n", file[i].f_name);
         }
     }
