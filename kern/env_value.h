dbafead76e0895e52c94fe0316901d8006f33381
管骁 <21373181@buaa.edu.cn>
2023-06-16 13:48:42 +0000

    删除env_value.h

--- kern/env_value.h
+++
@@ -1,4 +0,0 @@
-int create_shell_id();
-int declare_env_value(char *name, int value, int shell_id, int rdonly);
-int unset_env_value(char *name, int shell_id);
-int get_env_value(char *name, int shell_id, int op);
